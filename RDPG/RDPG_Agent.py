71534b23422a72e64c39e6ca12b7f3df9b5eabe7
ljk98116 <ljk98116@163.com>
2022-05-06 04:09:03 +0800

    RDPG implemnted successfully,use tanh to activate LSTM and actor net's last layer

--- RDPG/RDPG_Agent.py
+++ RDPG/RDPG_Agent.py
@@ -9,15 +9,15 @@ class RDPG_Agent:
     def __init__(
             self,
             env,
-            replay_size=80000,
-            episode=10000,
-            batch_size=64,
-            gamma=0.99,
+            replay_size=10000,
+            episode=300,
+            batch_size=32,
+            gamma=0.9,
             step=200,
-            T = 5,
-            hidden_units = 200,
-            learning_rate=1e-4,
-            update_freq=50
+            T = 10,
+            hidden_units = 30,
+            learning_rate=1e-3,
+            update_freq=1
     ):
         # env params
         self.env = env
@@ -52,6 +52,8 @@ class RDPG_Agent:
 
         # session
         self.sess = tf.Session()
+        self.state = tf.placeholder(tf.float32,[None,self.T,self.state_dim])
+        self.t_state = tf.placeholder(tf.float32,[None,self.T,self.state_dim])
 
         # nets and assign ops
         self.Actor()
@@ -67,6 +69,9 @@ class RDPG_Agent:
         # 初始化所有变量
         self.sess.run(tf.global_variables_initializer())
 
+        self.var = 3
+        self.render = 0
+
     @staticmethod
     def length(data):
         used = tf.sign(tf.reduce_max(tf.abs(data), reduction_indices=2))
@@ -76,32 +81,30 @@ class RDPG_Agent:
 
     def Actor(self):
         # batch_size x T x state_dim
-        self.kernel_initializer = tf.random_normal_initializer(0., 0.3)
-        self.bias_initializer = tf.constant_initializer(0.1)
-
         with tf.variable_scope('actor_net'):
             # forward process
-            self.actor_state = tf.placeholder(tf.float32, [None, self.T, self.state_dim], name='state')
+            self.actor_state = self.state
 
-            self.actor_lstm = rnn.LSTMCell(self.hidden_units)
+            self.actor_lstm = rnn.LSTMCell(self.hidden_units,activation='tanh')
             self.actor_init_state = self.actor_lstm.zero_state(self.batch_size, tf.float32)
             self.actor_lstm_outputs, self.actor_final_state = \
                 tf.nn.dynamic_rnn(self.actor_lstm, self.actor_state,
                                   sequence_length=self.length(self.actor_state),
                                   initial_state=self.actor_init_state, dtype=tf.float32)
             # T x batch_size x num_units
+            self.actor_lstm_outputs_list = tf.transpose(self.actor_lstm_outputs, [1, 0, 2])
             self.actor_lstm_outputs_list = \
                 tf.reshape(self.actor_lstm_outputs, [-1, self.hidden_units])
 
+
             actor_e1 = tf.layers.dense(
-                self.actor_lstm_outputs_list, 200, activation='relu',
-                kernel_initializer=self.kernel_initializer, bias_initializer=self.bias_initializer)
+                self.actor_lstm_outputs_list, 30, activation='relu')
 
             self.actor_output = tf.layers.dense(
-                actor_e1, self.action_dim,
-                kernel_initializer=self.kernel_initializer, bias_initializer=self.bias_initializer,
+                actor_e1, self.action_dim, activation='tanh'
             )
 
+            self.actor_output = tf.multiply(self.actor_output,self.action_dim)
             self.actor_output = tf.reshape(self.actor_output, [-1, self.T, self.action_dim])
             '''
             # 离散情况转换为softmax分布后进行采样
@@ -114,25 +117,25 @@ class RDPG_Agent:
         # batch_size x T x state_dim
         with tf.variable_scope('target_actor_net'):
             # forward process
-            self.t_actor_state = tf.placeholder(tf.float32, [None, self.T, self.state_dim], name='state')
+            self.t_actor_state = self.t_state
 
-            self.t_actor_lstm = rnn.LSTMCell(self.hidden_units)
+            self.t_actor_lstm = rnn.LSTMCell(self.hidden_units,activation='tanh')
             self.t_actor_init_state = self.t_actor_lstm.zero_state(self.batch_size, tf.float32)
             self.t_actor_lstm_outputs, self.t_actor_final_state = \
                 tf.nn.dynamic_rnn(self.t_actor_lstm, self.t_actor_state,
                                   sequence_length=self.length(self.t_actor_state),
                                   initial_state=self.t_actor_init_state, dtype=tf.float32)
             # T x batch_size x num_units
+            self.t_actor_lstm_outputs_list = tf.transpose(self.t_actor_lstm_outputs, [1, 0, 2])
+
             self.t_actor_lstm_outputs_list = \
                 tf.reshape(self.t_actor_lstm_outputs, [-1, self.hidden_units])
 
             t_actor_e1 = tf.layers.dense(
-                self.t_actor_lstm_outputs_list, 200, activation='relu',
-                kernel_initializer=self.kernel_initializer, bias_initializer=self.bias_initializer)
+                self.t_actor_lstm_outputs_list, 30, activation='relu')
 
             self.t_actor_output = tf.layers.dense(
-                t_actor_e1, self.action_dim,
-                kernel_initializer=self.kernel_initializer, bias_initializer=self.bias_initializer,
+                t_actor_e1, self.action_dim, activation='tanh'
             )
 
             self.t_actor_output = tf.reshape(self.t_actor_output, [-1, self.T, self.action_dim])
@@ -145,8 +148,8 @@ class RDPG_Agent:
 
     def Critic(self):
         with tf.variable_scope('critic_net'):
-            self.critic_state = tf.placeholder(tf.float32,[None,self.T,self.state_dim],name='critic_state')
-            self.critic_action = tf.placeholder(tf.float32,[None,self.T,self.action_dim],name='critic_action')
+            self.critic_state = self.state
+            self.critic_action = self.actor_output
 
             # 将state和action在维度上合并
             critic_state = tf.reshape(self.critic_state,[-1,self.state_dim])
@@ -155,28 +158,21 @@ class RDPG_Agent:
             self.critic_input = tf.concat([critic_state, critic_action], axis=1)
 
             e1 = tf.layers.dense(
-                self.critic_input,200,activation='relu',
-                kernel_initializer=self.kernel_initializer,
-                bias_initializer=self.bias_initializer
+                self.critic_input,30,activation='relu'
             )
 
             e2 = tf.layers.dense(
-                e1,200,activation='relu',
-                kernel_initializer=self.kernel_initializer,
-                bias_initializer=self.bias_initializer
+                e1,30,activation='relu',
             )
             self.critic_output = tf.layers.dense(
                 e2, 1,
-                kernel_initializer=self.kernel_initializer,
-                bias_initializer=self.bias_initializer
             )
             self.critic_output = tf.reshape(self.critic_output,[-1,self.T])
 
     def TargetCritic(self):
         with tf.variable_scope('target_critic_net'):
-            self.t_critic_state = tf.placeholder(tf.float32, [None, self.T, self.state_dim], name='target_critic_state')
-            self.t_critic_action = \
-                tf.placeholder(tf.float32, [None, self.T, self.action_dim], name='target_critic_action')
+            self.t_critic_state = self.t_state
+            self.t_critic_action = self.t_actor_output
 
             # 将state和action在维度上合并
             critic_state = tf.reshape(self.t_critic_state, [-1,self.state_dim])
@@ -184,47 +180,36 @@ class RDPG_Agent:
 
             self.t_critic_input = tf.concat([critic_state, critic_action], axis=1)
             e1 = tf.layers.dense(
-                self.t_critic_input,200,activation='relu',
-                kernel_initializer=self.kernel_initializer,
-                bias_initializer=self.bias_initializer
+                self.t_critic_input,30,activation='relu',
             )
             e2 = tf.layers.dense(
-                e1,200,activation='relu',
-                kernel_initializer=self.kernel_initializer,
-                bias_initializer=self.bias_initializer
+                e1,30,activation='relu',
             )
             self.t_critic_output = tf.layers.dense(
                 e2, 1,
-                kernel_initializer=self.kernel_initializer,
-                bias_initializer=self.bias_initializer
             )
             self.t_critic_output = tf.reshape(self.t_critic_output,[-1,self.T])
 
     def training_method(self):
         # Actor训练过程
         # batch_size x T
-        critic_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='critic_net')
-        actor_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope='actor_net')
+        critic_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='critic_net')
+        actor_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope='actor_net')
 
         self.critic_y_target = tf.placeholder(tf.float32, [None,self.T], name='critic_y_target')
-        self.critic_loss = tf.losses.mean_squared_error(self.critic_output, self.critic_y_target)
-        # critic网络的梯度
-        self.critic_grad = tf.gradients(self.critic_loss, critic_variables)
-        # critic关于输入critic的动作的梯度,需要critic_y_target,critic_state,critic_action(由actor网络输出)
-        self.critic_grad_ = tf.gradients(self.critic_loss, self.critic_action)
+        self.critic_loss = tf.losses.mean_squared_error( self.critic_y_target,self.critic_output)
         # 训练critic网络,需要critic_y_target,critic_state,critic_action(由replay_buffer抽样给出)
         self.critic_optimizer = tf.train.RMSPropOptimizer(self.lr)
-        self.critic_train_op = self.critic_optimizer.apply_gradients(zip(self.critic_grad, critic_variables))
+        self.critic_train_op = self.critic_optimizer.minimize(self.critic_loss,var_list=critic_variables)
         # 训练actor网络
-        self.critic_grad_input = tf.placeholder(tf.float32, [None, self.T, self.action_dim], name='critic_grad_input')
-        self.actor_grad = tf.gradients(self.actor_output, actor_variables, self.critic_grad_input)
+        self.actor_loss = -tf.reduce_mean(self.critic_output)
         self.actor_optimizer = tf.train.RMSPropOptimizer(self.lr)
-        self.actor_train_op = self.actor_optimizer.apply_gradients(zip(self.actor_grad,actor_variables))
+        self.actor_train_op = self.actor_optimizer.minimize(self.actor_loss,var_list=actor_variables)
 
     def softupdate(self):
         # actor network copy ops
-        actor_online_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='actor_net')
-        actor_target_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target_actor_net')
+        actor_online_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='actor_net')
+        actor_target_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_actor_net')
 
         for var in actor_online_params:
             self.actor_holder_list.append(tf.placeholder(tf.float32, [1] + var.shape.as_list()))
@@ -236,8 +221,8 @@ class RDPG_Agent:
         self.actor_update_net_op = tf.group(*self.actor_assign_ops)
 
         # critic network copy ops
-        critic_online_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='critic_net')
-        critic_target_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target_critic_net')
+        critic_online_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='critic_net')
+        critic_target_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_critic_net')
 
         for var in critic_online_params:
             self.critic_holder_list.append(tf.placeholder(tf.float32, [1] + var.shape.as_list()))
@@ -249,7 +234,7 @@ class RDPG_Agent:
         self.critic_update_net_op = tf.group(*self.critic_assign_ops)
 
     def update_net(self):
-        actor_online_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='actor_net')
+        actor_online_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='actor_net')
         for item, e in zip(self.actor_holder_list, actor_online_params):
             value = self.sess.run(e)
             value = value[np.newaxis, :]
@@ -257,7 +242,7 @@ class RDPG_Agent:
 
         self.sess.run(self.actor_update_net_op, feed_dict=self.actor_params_dict)
 
-        critic_online_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='critic_net')
+        critic_online_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='critic_net')
         for item, e in zip(self.critic_holder_list, critic_online_params):
             value = self.sess.run(e)
             value = value[np.newaxis, :]
@@ -277,11 +262,11 @@ class RDPG_Agent:
         self.replay_buffer.append([state,action,reward,next_state,done])
 
     def training(self):
-        if len(self.replay_buffer) > self.batch_size * self.T:
-            self._training()
-
-        if self.time_step % self.update_freq == 0:
+        if len(self.replay_buffer) >= self.replay_size:
             self.update_net()
+            self._training()
+            self.var *= 0.9995 ** self.T
+            self.lr *= 0.995
 
     # batch_size x T
     def sample(self):
@@ -314,28 +299,17 @@ class RDPG_Agent:
 
     def _training(self):
         state,action,reward,next_state,done = self.sample()
-        action_target = self.sess.run(self.t_actor_output,feed_dict={self.t_actor_state:next_state})
         Q_target = self.sess.run(
             self.t_critic_output,
-            feed_dict={self.t_critic_state:next_state, self.t_critic_action:action_target}
+            feed_dict={self.t_critic_state:next_state}
         )
 
         y_target = []
         for i in range(self.batch_size):
             y_target_T = []
             for j in range(self.T):
-                y_target_T.append(reward[i][j] + self.gamma * (1 - done[i][j]) * Q_target[i][j])
+                y_target_T.append(reward[i][j] + self.gamma * Q_target[i][j])
             y_target.append(y_target_T)
-        # 获取critic关于action的梯度
-        action_eval = self.sess.run(self.actor_output,feed_dict={self.actor_state:state})
-        critic_action_grad = self.sess.run(
-            self.critic_grad_,
-            feed_dict={
-                self.critic_y_target:y_target,
-                self.critic_state:state,
-                self.critic_action:action_eval
-            }
-        )[0]
 
         # 训练critic网络
         self.sess.run(
@@ -351,27 +325,28 @@ class RDPG_Agent:
         self.sess.run(
             self.actor_train_op,
             feed_dict={
-                self.actor_state:state,
-                self.critic_grad_input:critic_action_grad
+                self.actor_state:state
             }
         )
+        # print("actor_loss",self.sess.run(self.actor_loss,feed_dict={self.actor_state:state}))
+        # print("critic_loss",self.sess.run(self.critic_loss,feed_dict={self.critic_state:state,self.critic_y_target:y_target,self.critic_action:action}))
 
     def evaluate(self, ep):
         score = 0
-        for j in range(10):
+        for j in range(1):
             total_reward = 0
             state = self.env.reset()
             for step in range(self.STEP):
-                self.env.render()
+                if self.render:
+                    self.env.render()
                 action = self.choose_action(state)
                 next_state, reward, done, info = self.env.step(action)
                 total_reward += reward
-                if done:
-                    break
                 state = next_state
             score += total_reward
 
-        print('Episode:%d,Reward:%f' % (ep, score / 10.0))
+        if score >= -300: self.render = 1
+        print('Episode:%d,Reward:%f' % (ep, score))
 
     def fit(self):
         for ep in range(self.EPISODE):
@@ -379,17 +354,17 @@ class RDPG_Agent:
             self.time_step = 0
             for step in range(self.STEP):
                 action = self.choose_action(state)
+                action = np.clip(np.random.normal(action, self.var), -2, 2)
+
                 next_state, reward, done, info = self.env.step(action)
                 # reward = -1 if done else 0.1
-                self.store_transition(state, action, reward, next_state, done)
+                self.store_transition(state, action, reward / 10, next_state, done)
                 if self.time_step % self.T == 0:
                     self.training()
-                if done:
-                    break
                 state = next_state
                 self.time_step += 1
 
-            if ep % 50 == 0:
-                self.evaluate(ep)
+            self.evaluate(ep)
+            print(self.var)
 
 
